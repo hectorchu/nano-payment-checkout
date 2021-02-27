04ad54b908a9e4ad6c7d07f3bf386c30f163eccc
Hector Chu <hectorchu@gmail.com>
2021-02-27 12:33:07 +0000

    Add currency

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import queryString from 'query-string';
 
 export default function App() {
   const props = queryString.parse(window.location.search);
-  const {amount, title, on_success, on_error} = props;
+  const {api_url, amount, title, on_success, on_error} = props;
+  const currency = (props.currency || 'nano').toUpperCase();
+  const [currencyAmount, setCurrencyAmount] = useState('');
   const [show, setShow] = useState(true);
   const [err, setErr] = useState();
   const account = useNanoPayment({
     ...props,
-    apiURL: props.api_url,
+    apiURL: api_url,
     paymentID: props.payment_id,
     show,
     onSuccess: ({id}) => {
@@ -27,9 +29,17 @@ export default function App() {
     if (show || !on_error) return;
     window.location = `${on_error}&err=${err ? encodeURIComponent(err.message) : 'Cancelled'}`;
   }, [show]);
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch(`${api_url}/rates/?amount=1&currency=${currency}`);
+      const rate = await resp.text();
+      setCurrencyAmount((amount / rate).toFixed(2));
+    })();
+  }, [amount, currency]);
 
   if (title) document.title = title;
   if (!account) return null;
+  if (currency != 'NANO' && !currencyAmount) return null;
   return (
     <Container>
       <Row className="h-100 align-items-center justify-content-center">
@@ -37,9 +47,9 @@ export default function App() {
           <Card.Header as="h4">{title}</Card.Header>
           <Card.Body>
             <Card.Text>
-              Please send{' '}
-              <code className="border">{amount}</code>
-              {' '}NANO to <code>{account}</code>
+              Please send <code className="border">{amount}</code> NANO{' '}
+              {currency != 'NANO' && <small className="text-muted">({currencyAmount} {currency})</small>}
+              {' '}to <code>{account}</code>
             </Card.Text>
             <div className="text-center">
               <a href={nanoPaymentLink(account, amount)}>
